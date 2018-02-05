9a2866b6ba8c0a9e8f58020ef608d3a3f81b1d88
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-05 13:32:10 +0530

    Create ory.c

---
+++ ory.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+#define max 10
+
+int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
+int b[10];
+
+void merging(int low, int mid, int high) {
+   int l1, l2, i;
+
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= higheri++) {
+      if(a[l1] <= a[l2])
+         b[i] = a[l1++];
+      else
+         b[i] = a[l2++];
+   }
+   
+   while(l1 <= mid)    
+      b[i++] = a[l1++];
+
+   while(l2 <= high)   
+      b[i++] = a[l2++];
+
+   for(i = low; i <= high; i++)
+      a[i] = b[i];
+}
