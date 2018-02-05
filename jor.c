01083fbce63cf72a7976da49df1acdfbb3e38ee1
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-05 13:26:33 +0530

    Create jor.c

---
+++ jor.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#inlcude<conio.h>
+void main()
+                
+       int a,b;
+       printf("\n array of medium print":);
+       scanf("\d&a");
+       getch();
+       return 0;
+       }
