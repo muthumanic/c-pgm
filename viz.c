5697834c4338a06687aa76043f30904ee5fe0295
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-09 15:59:48 +0530

    Create viz.c

---
+++ viz.c
@@ -0,0 +1,13 @@
+#include<strdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numeric charcter ");
+scanf("d",&1);
+switch()
+case1:
+printf("\n numeric charcter of digits:);
+}
+getch();
+return 0;
