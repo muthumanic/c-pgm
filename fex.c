9498d3c27806374c9d2ae004d943c67e93b1f16d
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-12 13:44:56 +0530

    Create fex.c

---
+++ fex.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i,n,sum=0;
+clrscr();
+    n=10;
+    for(i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    printf("Sum: %d",add);
+getch();
+return 0;
+}
