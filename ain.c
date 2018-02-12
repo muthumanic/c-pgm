bdda7c1e1e86a43e110df80d479ae38ec7a14918
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-12 13:28:53 +0530

    Create ain.c

---
+++ ain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char s[1000], i;
+
+    printf("Enter a string: ");
+    scanf("%s","%d" s)
+
+    for(i = 0; s[i] != '\0'; ++i);
+
+    printf("Length of string: %d", i);
+    return 0;
+}
