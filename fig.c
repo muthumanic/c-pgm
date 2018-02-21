1f5779e8b053b1be905b78ba85e5c111453c7267
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-21 14:24:09 +0530

    Create fig.c

---
+++ fig.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\n Enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("\n one");
+break;
+case 2:printf("\n two");
+break;
+case 3:printf("\n three");
+break;
+case 4:printf("\n four");
+break;
+case 5:printf("\n five");
+break;
+case 6:printf("\n six");
+break;
+case 7:printf("\n seven");
+break;
+case 8:printf("\n eight");
+break;
+case 9:printf("\n nine");
+break;
+case 10:printf("\n ten");
+break;
+}
+getch();
+}
