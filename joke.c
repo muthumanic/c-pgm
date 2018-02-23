7761a1ab45dcdfd2e869687db10b320babde503a
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-23 15:55:02 +0530

    Create joke.c

---
+++ joke.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class charnum{
+     public static void main(String aa[]){
+     int n;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     String f=String.valueOf(n);
+     char a[]=f.toCharArray();
+     for(int i=0;i<a.length;i++){
+        int d=Character.getNumericValue(a[i]);
+        sum=sum+d;
+     }
+     System.out.print(sum);
