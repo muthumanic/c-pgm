366c016b37d1cb21bb8a206df89e35df8d9915ae
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-02-28 14:14:50 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+int count=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+if((n%i)==0)
+{
+count++;
+}
+}
+if(count==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+  return 0;
+}
