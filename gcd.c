9e7f8a0070c76598e5936f2b5dc0913bb6e8d8ff
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-03-09 16:02:30 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int a1,a2;
+printf("enter two number");
+scanf("%d %d",&a1,&a2);
+while(a1!=a2)
+{
+if(a1>a2)
+}
+else
+{
+a2=a2-a1;
+}
+printf("GCD %d",a1);
+}
+}
