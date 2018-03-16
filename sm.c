4c325015f5d76aabaf3880f980abe34afbc5ec8f
muthumanic <35688308+muthumanic@users.noreply.github.com>
2018-03-16 16:05:54 +0530

    Create sm.c

---
+++ sm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int num,len,p,i;
+	char str[100];
+    scanf("%s",str);
+	scanf("%d",&add);
+	len=strlen(str);
+	p=l-num;
+	for(i=p;i<l;i++)
+	{
+		printf("%c",string[i]);
+	}
+	return0;	
+}
